06d8f1fd858e8fd20baa6afd70cdd09829fb0ee9
lgatius <36736146+lgatius@users.noreply.github.com>
2018-02-22 18:15:26 +0100

    Update Prova.java

--- exerciciEclipse/src/exerciciEclipse/Prova.java
+++ exerciciEclipse/src/exerciciEclipse/Prova.java
@@ -4,7 +4,7 @@ public class Prova {
     
     public static void main(String[] args) {
         System.out.println("Hola");
-        System.out.println("Hola");
+        System.out.println("Que tal");
     }
 
 }
